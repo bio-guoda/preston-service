a8736c90ea97c2b35d85df06bd5fe11a26cab48b
jhpoelen <jhpoelen@xs4all.nl>
2020-11-04 14:10:19 -0800

    add registry api; related to https://github.com/bio-guoda/preston/issues/96

---
+++ preston-registry-service.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Reads an HTTP request from stdin, runs the preston command indicated in the request URI, and returns the output of that command.
+#
+# Designed for use with socat,
+#
+#   socat TCP4-LISTEN:8081,reuseaddr,fork SYSTEM:"./preston-registry-service.sh" 
+#
+# then e.g.
+#
+#   curl 'localhost:8081/arctos/http://arctos.database.museum/guid/UAM:Herb:74954'
+# 
+
+set -xe
+
+read -r REQUEST_METHOD REQUEST_URI REQUEST_HTTP_VERSION
+
+# Remove leading and trailing / characters from REQUEST_URI, split the result into CMD and ARGS
+IFS=/ read -r PATTERN_NAME IDENTIFIER_PATTERN <<<"$(echo $REQUEST_URI | sed -e 's:^/*::' -e 's:/*$::')"
+
+# Decide how to handle the request
+STATUS="200 OK"
+HEADER="Content-type: text/plain"
+
+# Send a response
+echo "HTTP/1.1 $STATUS"
+[ -n "$HEADER" ] && echo "$HEADER"
+echo
+[ -z "$FAIL" ] && ./find-datasets-with-link-pattern.sh $PATTERN_NAME $IDENTIFIER_PATTERN
+
