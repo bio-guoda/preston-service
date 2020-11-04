eac4bf6a59198cc6cb0c21129ff28c3975977e4d
jhpoelen <jhpoelen@xs4all.nl>
2020-11-04 14:28:42 -0800

    enable lookup

--- preston-registry-service.sh
+++ preston-registry-service.sh
@@ -37,5 +37,5 @@ esac
 echo "HTTP/1.1 $STATUS"
 [ -n "$HEADER" ] && echo "$HEADER"
 echo
-[ -z "$FAIL" ] && echo ./find-datasets-with-link-pattern.sh $PATTERN_NAME $IDENTIFIER_PATTERN
+[ -z "$FAIL" ] && ./find-datasets-with-link-pattern.sh $PATTERN_NAME $IDENTIFIER_PATTERN
 
