7e62258090291163ffc1cd394f73cdf9a9ad40ad
jhpoelen <jhpoelen@xs4all.nl>
2020-11-04 14:27:12 -0800

    fix command parsing

--- preston-registry-service.sh
+++ preston-registry-service.sh
@@ -8,7 +8,7 @@
 #
 # then e.g.
 #
-#   curl 'localhost:8081/arctos/http://arctos.database.museum/guid/UAM:Herb:74954'
+#   curl 'localhost:8081/find/arctos/http://arctos.database.museum/guid/UAM:Herb:74954'
 # 
 
 set -xe
@@ -16,15 +16,26 @@ set -xe
 read -r REQUEST_METHOD REQUEST_URI REQUEST_HTTP_VERSION
 
 # Remove leading and trailing / characters from REQUEST_URI, split the result into CMD and ARGS
-IFS=/ read -r PATTERN_NAME IDENTIFIER_PATTERN <<<"$(echo $REQUEST_URI | sed -e 's:^/*::' -e 's:/*$::')"
+IFS=/ read -r METHOD PATTERN_NAME IDENTIFIER_PATTERN <<<"$(echo $REQUEST_URI | sed -e 's:^/*::' -e 's:/*$::')"
 
 # Decide how to handle the request
 STATUS="200 OK"
 HEADER="Content-type: text/plain"
 
+case "$METHOD" in
+    find)
+        HEADER="Content-type: text/plain"
+        ;;
+    *)
+        STATUS="400 Bad Request"
+        FAIL=true
+        ;;
+esac
+
+
 # Send a response
 echo "HTTP/1.1 $STATUS"
 [ -n "$HEADER" ] && echo "$HEADER"
 echo
-[ -z "$FAIL" ] && ./find-datasets-with-link-pattern.sh $PATTERN_NAME $IDENTIFIER_PATTERN
+[ -z "$FAIL" ] && echo ./find-datasets-with-link-pattern.sh $PATTERN_NAME $IDENTIFIER_PATTERN
 
