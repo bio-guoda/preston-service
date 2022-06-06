e936fb5869a308e6077d1010c8640b8fbeed7321
jhpoelen <jhpoelen@xs4all.nl>
2022-06-06 11:51:19 -0500

    add BRIT tracking config

---
+++ preston-track-brit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# see https://github.com/bio-guoda/preston-brit-2022/blob/main/sample-1000-image-urls.sh
+#
+
+preston track https://sernecportal.org/portal/content/dwca/VDB_DwC-A.zip\
+ https://sernecportal.org/portal/content/dwca/NLU_DwC-A.zip\
+ https://portal.torcherbaria.org/portal/content/dwca/BRIT_DwC-A.zip\
+ | preston dwc-stream\
+ | grep URI\
+ | jq --raw-output '.["http://rs.tdwg.org/ac/terms/accessURI" | "http://rs.tdwg.org/ac/terms/thumbnailAccessURI" | "http://rs.tdwg.org/ac/terms/goodQualityAccessURI"]'\
+ > image-urls.tsv
+
+# tracks image urls
+
+cat image-urls.tsv | xargs -L25 preston track 
+ # randomly sample 1000 expected image urls
+ #shuf -n 1000 image-urls.tsv | xargs -L25 preston track 
