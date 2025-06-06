e0a2f27ba6a4931a4cba571182b3f22079906e86
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-06-06 16:47:58 -0500

    deduplicate

--- query/build-graph.sh
+++ query/build-graph.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #
-#  Load version preston provenance graph into oxigraph store.
-# 
-# see https://github.com/bio-guoda/preston/issues/199#issuecomment-1858618208
+# index provenance graph associated with an anchor
+# into oxigraph  
 #
 
+
 preston ls\
- --anchor hash://sha256/60e8810194e23f374b7c18947f5c37c56fe73b074f6617d3bdae845eab925fc1\                    
+ --anchor hash://sha256/a0d1df102236a0959daf70cd3cf5dd16ffdca043b53f4df08899adf883d85812\
  --remote https://linker.bio\
  | sed -E 's/(<)([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})([^ ]*)(>)/<urn:uuid:\2>/g'\
  | pv -l\
- | ../bin/oxigraph load --lenient --format nq --location preston-gib
+ | ./oxigraph_server_v0.3.22_x86_64_linux_gnu load --lenient --format nq --location preston-gib
+ 
+
