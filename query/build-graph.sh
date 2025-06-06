ed9effbe8e267abd97c947edec5b39affcf222e5
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-06-06 16:50:07 -0500

    use symlink instead

--- query/build-graph.sh
+++ query/build-graph.sh
@@ -10,6 +10,6 @@ preston ls\
  --remote https://linker.bio\
  | sed -E 's/(<)([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})([^ ]*)(>)/<urn:uuid:\2>/g'\
  | pv -l\
- | ./oxigraph_server_v0.3.22_x86_64_linux_gnu load --lenient --format nq --location preston-gib
+ | ../bin/oxigraph load --lenient --format nq --location preston-gib
  
 
