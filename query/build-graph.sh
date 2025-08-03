dfe59d4edaa1ec24529d59d60f07b87008dc0213
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-08-03 13:17:09 -0500

    refactor graph build script

--- query/build-graph.sh
+++ query/build-graph.sh
@@ -4,9 +4,10 @@
 # into oxigraph  
 #
 
+GIB_VERSION=hash://sha256/a0d1df102236a0959daf70cd3cf5dd16ffdca043b53f4df08899adf883d85812
 
 preston ls\
- --anchor hash://sha256/a0d1df102236a0959daf70cd3cf5dd16ffdca043b53f4df08899adf883d85812\
+ --anchor ${GIB_VERSION}\
  --remote https://linker.bio\
  | sed -E 's/(<)([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})([^ ]*)(>)/<urn:uuid:\2>/g'\
  | pv -l\
